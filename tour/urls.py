83807da617747e925c62fa650ba46653cd4785d2
sam <p.hansaem@hotmail.com>
2021-02-03 16:37:22 +0900

    Connect MySQL

--- tour/urls.py
+++ tour/urls.py
@@ -5,10 +5,10 @@ from . import views
 app_name = 'tour'
 
 urlpatterns = [
-    path('/login', views.login, name='login'),
-    path('/register', views.register, name='register'),
-    path('/search/<str:toursite_id>', views.search, name='search'),
-    path('/detail/<str:review_id>', views.detail, name='detail'),
-    path('/my-account', views.my_account, name='my_account'),
-    path('/my-list', views.my_list, name='my_list')
+    # path('/login', views.login, name='login'),
+    # path('/register', views.register, name='register'),
+    # path('/search/<str:toursite_id>', views.search, name='search'),
+    # path('/detail/<str:review_id>', views.detail, name='detail'),
+    # path('/my-account', views.my_account, name='my_account'),
+    # path('/my-list', views.my_list, name='my_list')
 ]
