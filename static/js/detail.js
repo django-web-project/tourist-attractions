ea9effa9b70077c179fb92759a7a1754e14fee19
sam <p.hansaem@hotmail.com>
2021-02-19 07:03:11 +0900

    Fix detail's text counter bug

--- static/js/detail.js
+++ static/js/detail.js
@@ -1,10 +1,10 @@
 function addTextInputEventListeners() {
     const newText= document.getElementsByClassName('new-textarea');
-    const remainingText = document.getElementsByClassName('text-remaining-chars')[0];
+    const remainingText = document.getElementsByClassName('text-remaining-chars');
     const MAX_CHARS = 100;
 
     function test() {
-        const remaining = MAX_CHARS - newText[0].value.length;
+        const remaining = MAX_CHARS - newText.value.length;
         const color = remaining < MAX_CHARS * 0.1 ? 'red' : null;
 
         remainingText.innerHTML = `${remaining}/100`;
@@ -12,13 +12,10 @@ function addTextInputEventListeners() {
 
         if(remaining <= 0) {
             alert('글자수는 100자로 제한됩니다.');
-            newText[0].value = newText[0].value.substring(0, 100);
-            newText[0].focus()
+            newText.value = newText[0].value.substring(0, 100);
+            newText.focus()
         }
     }
 
-    newText[0].addEventListener("input", () => {
-       test()
-    });
     test();
 }
